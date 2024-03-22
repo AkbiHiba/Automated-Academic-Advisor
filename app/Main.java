3e406244a7840f43ac6ab8a00d942a5661019262
yazidboufous <yazidbfs2004@gmail.com>
2024-03-23 00:21:46 +0200

    fixed the number of credits and courses

--- app/Main.java
+++ app/Main.java
@@ -28,39 +28,33 @@ public class Main {
                 Course eng202 = new Course("English", true, false, false, 3, new ArrayList<Course>());
                 Course las204 = new Course("Ethics", true, false, false, 3, new ArrayList<Course>());
                 Course bio209 = new Course("Basic Biology", true, false, false, 3, new ArrayList<Course>());
-                Course csc243L = new Course("CP1 LAB", true, true, true, 1, new ArrayList<Course>(),
-                                new ArrayList<Course>(Arrays.asList(csc243)));
                 Course csc245 = new Course("CP2", true, true, false, 3,
                                 new ArrayList<Course>(Arrays.asList(csc243)));
-                Course csc245L = new Course("CP2 LAB", true, true, true, 1, new ArrayList<Course>(),
-                                new ArrayList<Course>(Arrays.asList(csc245)));
                 Course mth207 = new Course("Discrete 1", true, true, false, 3, new ArrayList<Course>());
-                Course csc230 = new Course("CO", true, true, false, 3, new ArrayList<Course>(),
+                Course csc320 = new Course("CO", true, true, false, 3, new ArrayList<Course>(),
                                 new ArrayList<Course>(Arrays.asList(csc245, mth207)));
-                Course csc230L = new Course("CO LAB", true, true, true, 1, new ArrayList<Course>(),
-                                new ArrayList<Course>(Arrays.asList(csc230)));
+                Course csc320L = new Course("CO LAB", true, true, true, 1, new ArrayList<Course>(),
+                                new ArrayList<Course>(Arrays.asList(csc320)));
                 Course csc310 = new Course("CP3", true, true, false, 3,
                                 new ArrayList<Course>(Arrays.asList(csc245, mth207)));
-                Course csc310L = new Course("CP3 LAB", true, true, true, 1, new ArrayList<Course>(),
-                                new ArrayList<Course>(Arrays.asList(csc310)));
                 Course csc375 = new Course("Database Management", true, true, false, 3,
                                 new ArrayList<Course>(Arrays.asList(csc245, mth207)));
                 Course csc326 = new Course("Operating Systems", true, true, false, 3,
-                                new ArrayList<Course>(Arrays.asList(csc245, csc230)));
-                Course parallel = new Course("Parallel", true, true, false, 3,
+                                new ArrayList<Course>(Arrays.asList(csc245, csc320)));
+                Course csc447 = new Course("Parallel", true, true, false, 3,
                                 new ArrayList<Course>(Arrays.asList(csc326, csc310)));
-                Course csc340 = new Course("Software Engineering", true, true, false, 3,
+                Course csc490 = new Course("Software Engineering", true, true, false, 3,
                                 new ArrayList<Course>(), new ArrayList<Course>(Arrays.asList(csc375)));
-                Course csc380 = new Course("Discrete 2", true, true, false, 3,
+                Course mth307 = new Course("Discrete 2", true, true, false, 3,
                                 new ArrayList<Course>(Arrays.asList(mth207, mth201)));
                 Course csc430 = new Course("Networks", true, true, false, 3,
                                 new ArrayList<Course>(Arrays.asList(csc326)));
-                Course csc400 = new Course("Capstone", true, true, false, 3,
-                                new ArrayList<Course>(Arrays.asList(csc340)),
+                Course csc599 = new Course("Capstone", true, true, false, 3,
+                                new ArrayList<Course>(Arrays.asList(csc490)),
                                 new ArrayList<Course>(Arrays.asList(eng202, com203)));
                 Course mth305 = new Course("Statistics", true, true, false, 3,
                                 new ArrayList<Course>(Arrays.asList(mth201)));
-                Course csc500 = new Course("Professional Internship", true, true, false, 3,
+                Course csc491 = new Course("Professional Internship", true, true, false, 1,
                                 new ArrayList<Course>(), new ArrayList<Course>(Arrays.asList(csc375)));
                 Course csc1 = new Course("CS elective 1", true, true, false, 3,
                                 new ArrayList<Course>(Arrays.asList(csc310)));
@@ -78,7 +72,9 @@ public class Main {
                 Course hum2 = new Course("Humanities 2", true, false, false, 3, new ArrayList<Course>());
                 Course hum3 = new Course("Humanities 3", true, false, false, 3, new ArrayList<Course>());
                 Course hum4 = new Course("Humanities 4", true, false, false, 3, new ArrayList<Course>());
-                Course felec = new Course("Free Elective", true, false, false, 1, new ArrayList<Course>());
+                Course hum5 = new Course("Humanities 5", true, false, false, 3, new ArrayList<Course>());
+
+                Course felec = new Course("Free Elective", true, false, false, 3, new ArrayList<Course>());
 
                 // Nodes with edges of appropriate weights
 
@@ -86,49 +82,47 @@ public class Main {
                 Node nhum2 = new Node(hum2, new ArrayList<Edge>());
                 Node nhum3 = new Node(hum3, new ArrayList<Edge>());
                 Node nhum4 = new Node(hum4, new ArrayList<Edge>());
+                Node nhum5 = new Node(hum5, new ArrayList<Edge>());
+
                 Node nfelec = new Node(felec, new ArrayList<Edge>());
                 Node nlas204 = new Node(las204, new ArrayList<Edge>());
                 Node nbio209 = new Node(bio209, new ArrayList<Edge>());
-                Node ncsc243L = new Node(csc243L, new ArrayList<Edge>());
                 Node nmth1 = new Node(mth1, new ArrayList<Edge>());
-                Node ncsc380 = new Node(csc380, new ArrayList<Edge>());
+                Node nmth307 = new Node(mth307, new ArrayList<Edge>());
                 Node ncsc430 = new Node(csc430, new ArrayList<Edge>());
-                Node ncsc400 = new Node(csc400, new ArrayList<Edge>());
+                Node ncsc599 = new Node(csc599, new ArrayList<Edge>());
                 Node nmth305 = new Node(mth305, new ArrayList<Edge>());
-                Node ncsc500 = new Node(csc500, new ArrayList<Edge>());
+                Node ncsc491 = new Node(csc491, new ArrayList<Edge>());
                 Node ncsc1 = new Node(csc1, new ArrayList<Edge>());
                 Node ncsc2 = new Node(csc2, new ArrayList<Edge>());
                 Node ncsc3 = new Node(csc3, new ArrayList<Edge>());
                 Node ncsc4 = new Node(csc4, new ArrayList<Edge>());
                 Node ncsc5 = new Node(csc5, new ArrayList<Edge>());
-                Node nparallel = new Node(parallel, new ArrayList<Edge>());
-                Node ncsc340 = new Node(csc340,
-                                new ArrayList<Edge>(Arrays.asList(new Edge(ncsc400, 1), new Edge(ncsc500, 0))));
-                Node ncsc230L = new Node(csc230L, new ArrayList<Edge>());
-                Node ncsc310L = new Node(csc310L, new ArrayList<Edge>());
+                Node ncsc447 = new Node(csc447, new ArrayList<Edge>());
+                Node ncsc490 = new Node(csc490,
+                                new ArrayList<Edge>(Arrays.asList(new Edge(ncsc599, 1), new Edge(ncsc491, 0))));
+                Node ncsc320L = new Node(csc320L, new ArrayList<Edge>());
                 Node ncsc310 = new Node(csc310,
                                 new ArrayList<Edge>(Arrays.asList(new Edge(ncsc1, 1), new Edge(ncsc2, 1),
-                                                new Edge(ncsc310L, 0),
                                                 new Edge(ncsc3, 1), new Edge(ncsc4, 1), new Edge(ncsc5, 1),
-                                                new Edge(nparallel, 1))));
+                                                new Edge(ncsc447, 1))));
                 Node ncsc375 = new Node(csc375,
-                                new ArrayList<Edge>(Arrays.asList(new Edge(ncsc500, 0), new Edge(ncsc340, 0))));
+                                new ArrayList<Edge>(Arrays.asList(new Edge(ncsc491, 0), new Edge(ncsc490, 0))));
 
                 Node ncsc326 = new Node(csc326,
-                                new ArrayList<Edge>(Arrays.asList(new Edge(nparallel, 1), new Edge(ncsc430, 1))));
-                Node ncsc230 = new Node(csc230,
-                                new ArrayList<Edge>(Arrays.asList(new Edge(ncsc326, 1), new Edge(ncsc230L, 0))));
-                Node ncsc245L = new Node(csc245L, new ArrayList<Edge>());
+                                new ArrayList<Edge>(Arrays.asList(new Edge(ncsc447, 1), new Edge(ncsc430, 1))));
+                Node ncsc320 = new Node(csc320,
+                                new ArrayList<Edge>(Arrays.asList(new Edge(ncsc326, 1), new Edge(ncsc320L, 0))));
                 Node ncsc245 = new Node(csc245,
-                                new ArrayList<Edge>(Arrays.asList(new Edge(ncsc310, 1), new Edge(ncsc245L, 0),
-                                                new Edge(ncsc326, 1), new Edge(ncsc375, 1), new Edge(ncsc230, 0))));
-                Node nmth207 = new Node(mth207, new ArrayList<Edge>(Arrays.asList(new Edge(ncsc380, 1),
-                                new Edge(ncsc375, 1), new Edge(ncsc310, 1), new Edge(ncsc230, 0))));
+                                new ArrayList<Edge>(Arrays.asList(new Edge(ncsc310, 1),
+                                                new Edge(ncsc326, 1), new Edge(ncsc375, 1), new Edge(ncsc320, 0))));
+                Node nmth207 = new Node(mth207, new ArrayList<Edge>(Arrays.asList(new Edge(nmth307, 1),
+                                new Edge(ncsc375, 1), new Edge(ncsc310, 1), new Edge(ncsc320, 0))));
                 Node ncsc243 = new Node(csc243, new ArrayList<Edge>(Arrays.asList(new Edge(ncsc245, 1))));
                 Node nmth201 = new Node(mth201, new ArrayList<Edge>(
-                                Arrays.asList(new Edge(nmth1, 1), new Edge(nmth305, 1), new Edge(ncsc380, 1))));
-                Node ncom203 = new Node(com203, new ArrayList<Edge>(Arrays.asList(new Edge(ncsc400, 0))));
-                Node neng202 = new Node(eng202, new ArrayList<Edge>(Arrays.asList(new Edge(ncsc400, 0))));
+                                Arrays.asList(new Edge(nmth1, 1), new Edge(nmth305, 1), new Edge(nmth307, 1))));
+                Node ncom203 = new Node(com203, new ArrayList<Edge>(Arrays.asList(new Edge(ncsc599, 0))));
+                Node neng202 = new Node(eng202, new ArrayList<Edge>(Arrays.asList(new Edge(ncsc599, 0))));
                 Graph g = new Graph();
 
                 g.addNode(ncsc243);
@@ -136,29 +130,28 @@ public class Main {
                 g.addNode(nhum2);
                 g.addNode(nhum3);
                 g.addNode(nhum4);
+                g.addNode(nhum5);
+
                 g.addNode(nfelec);
                 g.addNode(nmth201);
                 g.addNode(ncom203);
                 g.addNode(neng202);
                 g.addNode(nlas204);
                 g.addNode(nbio209);
-                g.addNode(ncsc243L);
                 g.addNode(ncsc245);
-                g.addNode(ncsc245L);
                 g.addNode(nmth207);
-                g.addNode(ncsc230);
-                g.addNode(ncsc230L);
+                g.addNode(ncsc320);
+                g.addNode(ncsc320L);
                 g.addNode(ncsc310);
-                g.addNode(ncsc310L);
                 g.addNode(ncsc375);
                 g.addNode(ncsc326);
-                g.addNode(nparallel);
-                g.addNode(ncsc340);
-                g.addNode(ncsc380);
+                g.addNode(ncsc447);
+                g.addNode(ncsc490);
+                g.addNode(nmth307);
                 g.addNode(ncsc430);
-                g.addNode(ncsc400);
+                g.addNode(ncsc599);
                 g.addNode(nmth305);
-                g.addNode(ncsc500);
+                g.addNode(ncsc491);
                 g.addNode(ncsc1);
                 g.addNode(nmth1);
                 g.addNode(ncsc2);
